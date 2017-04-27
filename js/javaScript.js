3a1d75bd5d2f7ce6c90cb743bf4492113fc464df
Manuel Cascallar <manuel.cascallar@avature.net>
2017-04-27 12:19:04 -0300

    Just make things alright.

--- js/javaScript.js
+++ js/javaScript.js
@@ -4,35 +4,24 @@ $(document).ready(function(){
 	$("#demosMenu").change(function(){
 	  window.location.href = $(this).find("option:selected").attr("id") + '.html';
 	});
-});
 
-
-		$(document).ready(function() {
-			$('#fullpage').fullpage({
-				sectionsColor: ['black', '#1BBC9B', '#7E8F7C'],
-				scrollBar: false,
-				navigation: true,
-				navigationPosition: 'right',
-				navigationTooltips: ['Somos más', 'Unite gratis', 'Sumate', 'Recibí', 'Mail',],
-				verticalCentered: false,
-			});
-		});
-
-
-
-
-// animate - section0
-
-
-
-
-// animate - section1
-
-$(document).ready(function(){
-    var aboutEl = $('image');
+    $('#fullpage').fullpage({
+		sectionsColor: ['black', '#1BBC9B', '#7E8F7C'],
+		scrollBar: false,
+		navigation: true,
+		navigationPosition: 'right',
+		navigationTooltips: ['Somos más', 'Unite gratis', 'Sumate', 'Recibí', 'Mail',],
+		verticalCentered: false,
+	});
+    var aboutEl = $('.image');
     var aboutElOffset = aboutEl.offset().top/1.1;
     var documentEl = $(document);
     documentEl.on('scroll', function() {
         if( documentEl.scrollTop() > aboutElOffset && aboutEl.hasClass('hidden')) aboutEl.removeClass("hidden")
     });
 });
+
+
+
+
+// animate - section1
