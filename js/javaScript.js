7cb04533fd16627adbc96dd3659c875f321e2bec
marcos.fersu <marcos.fersu@gmail.com>
2017-04-27 09:55:38 -0300

    add new brach, animate

--- js/javaScript.js
+++ js/javaScript.js
@@ -1,3 +1,5 @@
+// full Page
+
 $(document).ready(function(){
 	$("#demosMenu").change(function(){
 	  window.location.href = $(this).find("option:selected").attr("id") + '.html';
@@ -15,3 +17,37 @@ $(document).ready(function(){
 				verticalCentered: false,
 			});
 		});
+
+
+
+
+// animate - section0
+
+
+(function() {
+
+	var aboutEl = $('div.image'),
+	//	aboutElOffset = aboutEl.offset().top/1.1,
+	//	aboutElOffset = aboutEl.offset(),
+		documentEl = $(document);
+
+		documentEl.on('scroll', function() {
+			if( aboutEl.hasClass('hidden')) aboutEl.removeClass('hidden')
+		});
+
+})();
+
+
+// animate - section1
+
+(function() {
+
+	var aboutEl = $('div.row'),
+		aboutElOffset = aboutEl.offset().top/1.1,
+		documentEl = $(document);
+
+		documentEl.on('scroll', function() {
+			if( documentEl.scrollTop() > aboutElOffset && aboutEl.hasClass('hidden')) aboutEl.removeClass("hidden")
+		});
+
+})();
