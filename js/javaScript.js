fa6ade1edc48bff374db5438365674e7c55a4d2c
marcos.fers <marcos.fersu@gmail.com>
2017-05-30 16:52:33 -0300

    change and add the littles tags.

--- js/javaScript.js
+++ js/javaScript.js
@@ -45,48 +45,66 @@ $(document).ready(function(){
 
 	});
 
+	// 	$(".bottomVideo").click(function(){
+	// 			$(".row").removeClass("videoLeft");
+	// 			$(".image").removeClass("videoRight");
+	// 			$(".video").removeClass("hidden5");
+	// 			$(".video").append('<iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/HIAMc5pHxcA?rel=0&autoplay=1&amp;controls=0&amp;showinfo=0?ecver=1" frameborder="0" allowfullscreen></iframe>');
+	// 			$(".bottomVideo").removeClass("none");
+	// 	});
+
+	// });
+
+
+// YouTube
+
 	$(".bottomVideo").click(function(){
 			$(".row").removeClass("videoLeft");
 			$(".image").removeClass("videoRight");
-			$(".video").removeClass("hidden5");
-			$(".video").append('<iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/HIAMc5pHxcA?rel=0&autoplay=1&amp;controls=0&amp;showinfo=0?ecver=1" frameborder="0" allowfullscreen></iframe>');
 			$(".bottomVideo").removeClass("none");
-	});
+		setTimeout(function(){
+			$(".video").removeClass("hidden7");
+		}, 900)
+		
+//			$(".video").append('<iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/HIAMc5pHxcA?rel=0&autoplay=1&amp;controls=0&amp;showinfo=0?ecver=1" frameborder="0" allowfullscreen></iframe>');
+
+    });
 
 });
 
+      var tag = document.createElement('script');
 
-    // var aboutEl = $('.image');
-    // var aboutElOffset = aboutEl.offset().top/1.1;
-    // var documentEl = $(document);
-    // documentEl.on('scroll', function() {
-    //     if( documentEl.scrollTop() > aboutElOffset && aboutEl.hasClass('hidden')){
-    //     aboutEl.removeClass("hidden")}
-    // });
+      tag.src = "https://www.youtube.com/iframe_api";
+      var firstScriptTag = document.getElementsByTagName('script')[0];
+      firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
 
-// animate - section1
-
-/*	
-	$(".bottomVideo").click(function(){
-
-		var videoOn = 0;
-
-		if(videoOn = 0){
-			$(".videoLeft").css("left", "-=699");
-			$(".videoRight").css("left", "+=700");
-			videoOn = 1;
-
-		}else(videoOn == 1){
-			$(".videoLeft").css("left", "+=699");
-			$(".videoRight").css("left", "-=700");
-			videOn == 0;
-		}
-	});
 
 
-	$(".bottomVideo").click(function(){
-			$(".videoLeft").css("left", "-=699")
-			$(".videoRight").css("left", "+=700")
-	});
-	*/
+var player;
+      function onYouTubeIframeAPIReady() {
+        player = new YT.Player('player', {
+          height: '360',
+          width: '640',
+          videoId: 'HIAMc5pHxcA',
+          events: {
+            'onReady': onPlayerReady,
+          }
+        });
+      }
+
+
+      function onPlayerReady(event) {
+        
+        // bind events
+        var playButton = document.getElementById("play-button");
+        playButton.addEventListener("click", function() {
+          player.playVideo();
+        });
+        
+        var pauseButton = document.getElementById("pause-button");
+        pauseButton.addEventListener("click", function() {
+          player.pauseVideo();
+        });
+        
+      }
